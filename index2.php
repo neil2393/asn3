0620f480a4e8ee793d563606a561f6d9834eb819
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 16:16:21 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -39,8 +39,8 @@ include 'getcustomerdata.php';
 
 <div class="row">
 <div class="col s12 m6">
-  <div class="card">
-    <div class="card-content">
+  <div class="card blue-grey darken-1">
+    <div class="card-content white-text">
       <span class="card-title">Card Title</span>
       <p>I am a very simple card. I am good at containing small bits of information.
       I am convenient because I require little markup to use effectively.</p>
