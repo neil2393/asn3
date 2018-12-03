fe94eebb883ae85a40eb6df9c34058fff851b6df
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 17:39:33 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -10,10 +10,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.dropdown-trigger');
-        var instances = M.Dropdown.init(elems, options);
-      });
+      $('.dropdown-trigger').dropdown();
     </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   </head>
