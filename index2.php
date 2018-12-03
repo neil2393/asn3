4329afd5b51b07c2aee8e682158e3db9c62555e0
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 17:16:31 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -7,9 +7,23 @@ table, th, td {
     border: 1px solid black;
 }
 </style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+<script>
+    $( document ).ready(function() {
+        $('.dropdown-button').dropdown({
+          inDuration: 300,
+          outDuration: 225,
+          constrain_width: false, // Does not change width of dropdown to that of the activator
+          hover: false, // Activate on click
+          alignment: 'left', // Aligns dropdown to left or right edge (works with constrain_width)
+          gutter: 0, // Spacing from edge
+          belowOrigin: false // Displays dropdown below the button
+        });
+    });
+</script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
-
 
 
 
@@ -35,15 +49,6 @@ include 'connectdb.php';
 <?php
 include 'getcustomerdata.php';
 
-$('.dropdown-button').dropdown({
-  inDuration: 300,
-  outDuration: 225,
-  constrain_width: false, // Does not change width of dropdown to that of the activator
-  hover: true, // Activate on hover
-  gutter: 0, // Spacing from edge
-  belowOrigin: false // Displays dropdown below the button
-}
-);
 ?>
 <h5>Products that a customer has purchased:</h5>
 
