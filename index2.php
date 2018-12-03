da504c25dfe4f836e3f049a5b03df8789322f4c5
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 16:46:04 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -37,6 +37,20 @@ include 'getcustomerdata.php';
 ?>
 <h5>Products that a customer has purchased:</h2>
 
+<!-- Dropdown Trigger -->
+<a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
+
+<!-- Dropdown Structure -->
+<ul id='dropdown1' class='dropdown-content'>
+<li><a href="#!">one</a></li>
+<li><a href="#!">two</a></li>
+<li class="divider" tabindex="-1"></li>
+<li><a href="#!">three</a></li>
+<li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+<li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+</ul>
+
+
 <div class="row">
 <div class="col s12 m6">
   <div class="card blue-grey darken-1">
