fb067906c80fc1ff6f3a66b6a55d0619569180cb
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-02 21:10:19 -0500

    push script files

--- getcustomerdata.php
+++ getcustomerdata.php
@@ -4,11 +4,28 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
      die("databases query failed.");
 }
-echo "<ol>";
+echo "<table>";
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<ol>";
-    echo $row["firstName"] . " " . $row["lastName"] . " " . $row["city"] . " " . $row["phoneNumber"] . " " . $row["customerId"] . " " . $row["agentId"] . "</ol>";
+    echo "<tr>";
+    echo "<td>" . $row["firstName"] . "</th><td>" . $row["lastName"] . "</th><td>" . $row["city"] . "</th><td>" . $row["phoneNumber"] . "</th><td>" . $row["customerId"] . "</th><td>" . $row["agentId"] . "</td>";
+    echo "</tr>"
 }
 mysqli_free_result($result);
-echo "</ol>";
+echo "</table>";
 ?>
+
+
+<table>
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+  <tr>
+    <td>January</td>
+    <td>$100</td>
+  </tr>
+  <tr>
+    <td>February</td>
+    <td>$80</td>
+  </tr>
+</table>
--- index2.php
+++ index2.php
@@ -3,14 +3,19 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Dr. Western's Vet Clinic</title>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+<title>CS3319 Assignment 3</title>
 </head>
 <body>
 <?php
 include 'connectdb.php';
 ?> 
-<h1>Welcome to the Western Vet Clinic</h1>
-<h2>Pets we look after</h2>
+<h1>CS3319 Assignment 3</h1>
+<h2>Customer Information</h2>
 <?php
 include 'getcustomerdata.php';
 ?>
