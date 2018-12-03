08ae845b604ad848a2be9e65c216543e52277b93
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 17:30:57 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -1,5 +1,12 @@
+<!DOCTYPE html>
 <html> 
   <head>
+  <meta charset="utf-8">
+  <style>
+    table, th, td {
+    border: 1px solid black;
+    }
+  </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
     <!-- This script block could also be in an external .js file -->
@@ -29,4 +36,48 @@
     <li class="divider"></li>
     <li><a href="#!">three</a></li>
   </ul>
+
+  <body>
+
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">CS3319 Assignment Neil Patel</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <!--
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+    -->
+      </ul>
+    </div>
+    </nav>
+
+    <?php
+    include 'connectdb.php';
+    ?> 
+
+    <h5>Customer Information:</h5>
+
+    <?php
+    include 'getcustomerdata.php';
+    ?>
+
+    <h5>Products that a customer has purchased:</h5>
+
+  </body>
 </html>
+
+<?php
+/*
+ssh -i ~/.ssh/cs3319-vm144.pem vm144@cs3319.gaul.csd.uwo.ca
+ssh centos@vm144
+mysql -u root -p
+cd /var/www/html
+
+
+git add .
+git commit -m "push scripts files"
+git push origin master
+*/
+?>
+  </html>
