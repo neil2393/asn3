870435aaa850ca89ec5770c579a821ac0dd8b8a9
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 14:04:36 -0500

    push scripts files

--- getcustomerdata.php
+++ getcustomerdata.php
@@ -14,19 +14,3 @@ while ($row = mysqli_fetch_assoc($result)) {
 mysqli_free_result($result);
 echo "</table>";
 ?>
-
-
-<table>
-  <tr>
-    <th>Month</th>
-    <th>Savings</th>
-  </tr>
-  <tr>
-    <td>January</td>
-    <td>$100</td>
-  </tr>
-  <tr>
-    <td>February</td>
-    <td>$80</td>
-  </tr>
-</table>
