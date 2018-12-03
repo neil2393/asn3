695c1128908b37f213154f78c9e7c568515910f8
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 13:57:42 -0500

    push script files

--- getcustomerdata.php
+++ getcustomerdata.php
@@ -7,7 +7,8 @@ if (!$result) {
 echo "<table>";
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>";
-    echo "<td>" . $row["firstName"] . "</th><td>" . $row["lastName"] . "</th><td>" . $row["city"] . "</th><td>" . $row["phoneNumber"] . "</th><td>" . $row["customerId"] . "</th><td>" . $row["agentId"] . "</td>";
+    echo "<tr><th>First Name</th><th>Last Name</th><th>City</th><th>Phone Number</th><th>Customer ID</th><th>Agent ID</th></tr>"
+    echo "<td>" . $row["firstName"] . "</td><td>" . $row["lastName"] . "</td><td>" . $row["city"] . "</td><td>" . $row["phoneNumber"] . "</td><td>" . $row["customerId"] . "</td><td>" . $row["agentId"] . "</td>";
     echo "</tr>";
 }
 mysqli_free_result($result);
