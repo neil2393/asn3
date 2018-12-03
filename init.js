25187ac27b57d267b3eb191a33a45c9ae23928f9
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 17:03:01 -0500

    push scripts files

--- init.js
+++ init.js
@@ -1,4 +1,12 @@
+window.onload = function() {
+ prepareListener();
+}
+
+function prepareListener() {
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
-  });
+  
+}
+
