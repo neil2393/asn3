307717075c2e4364cec00fc19416c118eb8abb9a
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 17:27:38 -0500

    push scripts files

--- getpurchasedata.php
+++ getpurchasedata.php
@@ -42,7 +42,7 @@
               $selected_category = $_GET['category'];
               $_SESSION['selected_category'] = $_GET['category'];
 
-              $query = "SELECT * FROM products, purchases WHERE products.productId =purchases.productId AND product_id IN (SELECT purchases.productId FROM purchases, customers WHERE purchases.customerId = customers.customerId AND ";
+              $query = "SELECT * FROM products, purchases WHERE products.productId =purchases.productId AND productId IN (SELECT purchases.productId FROM purchases, customers WHERE purchases.customerId = customers.customerId AND ";
               $query .= "customers.customerId = " . $selected_category . "))";
               $result = mysqli_query($connection,$query);
               if (!$result) {
