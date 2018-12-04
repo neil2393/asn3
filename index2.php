fd84611e0832442db7181b77fe0f7718cf040ba8
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 04:35:00 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -33,24 +33,17 @@
     include 'connectdb.php';
     ?> 
 
-          
-
-
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card">
-          <div class="card-content">
-            <br>
-            <h5>Customer Information:</h5>
-            <div class="row">
-              <div class="col s12 m6">
-                <div class="card blue-grey darken-1">
-                  <div class="card-content white-text">
-                    <?php
-                    include 'getcustomerdata.php';
-                    ?>
-                  </div>
-                </div>
+    <div class="card">
+      <div class="card-content">
+        <br>
+        <h5>Customer Information:</h5>
+        <div class="row">
+          <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <?php
+                include 'getcustomerdata.php';
+                ?>
               </div>
             </div>
           </div>
