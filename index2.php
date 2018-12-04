a7ba08d3d9d690c327a5dc709fe57919a411d36b
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-03 20:09:41 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -74,21 +74,7 @@
     </div>
 
     <?php
-    $query = "SELECT * FROM customers ORDER BY lastName";
-    $result = mysqli_query($connection,$query);
-    if (!$result) {
-         die("databases query failed.");
-    }
-    echo "<table>";
-    echo "<tr><th>Customer ID</th><th>First Name</th><th>Last Name</th><th>City</th><th>Phone Number</th><th>Agent ID</th></tr>";
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["customerId"] . "</td><td>" . $row["firstName"] . "</td><td>" . $row["lastName"] . "</td><td>" . $row["city"] . "</td><td>" . $row["phoneNumber"] . "</td><td>" . $row["agentId"] . "</td>";
-        echo "</tr>";
-    }
-    mysqli_free_result($result);
-    mysqli_close($connection);
-    echo "</table>";
+      include 'getcustomerdata.php';
     ?>
 
   </body>
