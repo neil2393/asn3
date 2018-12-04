1599684ef0b5de895e4b80a3e54b04273c634ce9
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 05:52:02 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -98,10 +98,10 @@
             <div class="card-content white-text">
               <span class="card-title">New Purchase</span>
               <p>Sort product information in the following orders:</p>
-            </div>
-            <div class="input-field col s6">
-              <input id="last_name" type="text" class="validate">
-              <label for="last_name">Last Name</label>
+                <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">Last Name</label>
+                </div>
             </div>
           </div>
         </div>
