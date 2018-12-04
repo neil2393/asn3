8113ca7bfd18a89754b5ae0eecb131b8f81598b5
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 04:06:02 -0500

    push scripts files

--- getpurchasedata.php
+++ getpurchasedata.php
@@ -8,13 +8,13 @@
       }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
       $(document).ready(function(){
         $('.sidenav').sidenav();
       });
     </script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   </head>
 
   <body>
