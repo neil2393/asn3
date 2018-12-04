ce8828ef1a4e7a47085be635c1aaba9a4be624fa
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 05:55:18 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -106,7 +106,6 @@
                 <input id="last_name" type="text" class="validate">
                 <label for="last_name">Last Name</label>
                 </div>
-              <p>test</p>
             </div>
           </div>
         </div>
