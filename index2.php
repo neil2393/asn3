d16d387d10b5498da184210a1dd11e8545225cc0
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 19:03:12 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -121,50 +121,46 @@
         </div>
       </div>
 
-<!--
-    <div class="row">
-      <form class="col s12">
-        <div class="row">
-          <div class="input-field col s6">
-            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-            <label for="first_name">First Name</label>
-          </div>
-          <div class="input-field col s6">
-            <input id="last_name" type="text" class="validate">
-            <label for="last_name">Last Name</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-            <label for="disabled">Disabled</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input id="password" type="password" class="validate">
-            <label for="password">Password</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input id="email" type="email" class="validate">
-            <label for="email">Email</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col s12">
-            This is an inline input field:
-            <div class="input-field inline">
-              <input id="email_inline" type="email" class="validate">
-              <label for="email_inline">Email</label>
-              <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+      <blockquote><h5>Insert New Customer Information:</h5></blockquote>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">New Customer</span>
+              <p>Enter the following information to enter a new customer:</p>
+              <form action="newcustomer.php" method="post">
+                  <div class="input-field blue-grey darken-1">
+                  <input name="customerId" type="text" class="validate">
+                  <label for="customerId">Customer ID</label>
+                  </div>
+                  <div class="input-field blue-grey darken-1">
+                  <input name="firstName" type="text" class="validate">
+                  <label for="firstName">First Name</label>
+                  </div>
+                  <div class="input-field blue-grey darken-1">
+                  <input name="lastName" type="text" class="validate">
+                  <label for="lastName">Last Name</label>
+                  </div>
+                  <div class="input-field blue-grey darken-1">
+                  <input name="city" type="text" class="validate">
+                  <label for="city">City</label>
+                  </div>
+                  <div class="input-field blue-grey darken-1">
+                  <input name="phoneNumber" type="text" class="validate">
+                  <label for="phoneNumber">Phone Number</label>
+                  </div>
+                  <div class="input-field blue-grey darken-1">
+                  <input name="agentId" type="text" class="validate">
+                  <label for="agentId">Agent ID</label>
+                  </div>
+              <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                <i class="material-icons right">send</i>
+              </button>
+              </form>
             </div>
           </div>
         </div>
-      </form>
-    </div>
--->
+      </div>
 
     <?php
       mysqli_close($connection);
---
+++ newcustomer.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html> 
+  <head>
+    <meta charset="utf-8">
+    <style>
+      table, th, td {
+      border: 1px solid black;
+      }
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+    <script>
+      $('.dropdown-trigger').dropdown();
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+  </head>
+
+  <body>
+    <nav>
+    <div class="nav-wrapper grey darken-3">
+      <a href="#" class="brand-logo center">CS3319 Assignment 3 Neil Patel</a>
+    <!--
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    -->
+    </div>
+    </nav>
+
+    <br>
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <?php
+              session_start();
+              include 'connectdb.php';
+
+              $customerId = (int)$_POST["customerId"];
+              $firstName = (int)$_POST["firstName"];
+              $lastName = (int)$_POST["lastName"];
+              $city = (int)$_POST["city"];
+              $phoneNumber = (int)$_POST["phoneNumber"];
+              $agentId = (int)$_POST["agentId"];
+
+              $query = "SELECT * FROM customers";
+              $result = mysqli_query($connection,$query);
+              if (!$result) {
+                die("databases query failed.");
+              }
+              while ($row = mysqli_fetch_assoc($result)) {
+                if ($row["customerId"] == $customerId) {
+                  echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
+                  die("Error - You cannot use an existing customer ID. Please try again.");
+                }
+              }
+              
+              $query1 = 'INSERT INTO customers (customerId, firstName, lastName, city, phoneNumber, agentId) VALUES ("' . $customerId . '", "' . $firstName . '", "' . $lastName . '", "' . $city . '", "' . $phoneNumber . '", "' . $agentId . '");';
+              if (!mysqli_query($connection, $query)) {
+                die("Error -- insert failed: " . mysqli_error($connection));
+              }
+              echo 'Your new customer was successfully added.';
+
+/*
+              $check = 0;
+              $query = 'SELECT * FROM purchases WHERE customerId = '. $customerId . ' AND productId = ' . $productId;
+              $result = mysqli_query($connection, $query);
+              if (!$result) {
+                  echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
+                  die("Error<br>" . mysqli_error($connection));
+              }
+              while ($row = mysqli_fetch_assoc($result)) {
+                  // If purchase exists, value of $purchase_exists will be > 0
+                  $check += $row['customerId'];
+              }
+              mysqli_free_result($result);
+
+              // If already purchased, update value
+              if($check > 0) {
+                  // Makes sure they are adding and not deducting
+                  if($quantity > 0){
+                      $query = 'UPDATE purchases SET quantity = quantity + ' . $quantity . ' WHERE customerId = '. $customerId . ' AND productId = ' . $productId;
+                      if (!mysqli_query($connection, $query)) {
+                          echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
+                          die("Error.<br>" . mysqli_error($connection));
+                      }
+                      echo "Your new purchase was successfuly updated.";
+                  }
+                  else {
+                    //  echo "Quantity should be positive!";
+                      echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
+                      die("Error - You can only enter positive quantities.");
+                  }
+              }
+              // Otherwise, insert values into purchases
+              else {
+                  $query = 'INSERT INTO purchases(customerId, productId, quantity) VALUES(' . $customerId . ',' . $productId . ',"' . $quantity . '")';
+                  if (!mysqli_query($connection, $query)) {
+                      echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
+                      die("Error - Either the Customer ID or Product ID is incorrect.<br>" . mysqli_error($connection));
+                  }
+                  echo "Your new purchase was successfuly added.";
+              }
+
+  */
+              mysqli_close($connection);
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <?php
+      echo "<blockquote><h5>Updated Customer Information:</h5></blockquote>"
+    ?>
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <?php
+              session_start();
+              include 'connectdb.php';
+
+              $customerId = (int)$_POST["customerId"];
+              $productId = (int)$_POST["productId"];
+              $quantity = (int)$_POST["quantity"];
+
+              $query = "SELECT * FROM products, purchases, customers WHERE products.productId = purchases.productId AND purchases.customerId = customers.customerId AND customers.customerId = " . $customerId;
+              //$query .= "customers.customerId = " . $customerId . ")";
+              $result = mysqli_query($connection,$query);
+              if (!$result) {
+                  die("databases query failed.");
+              }
+              echo "<table>";
+              echo "<tr><th>Product ID</th><th>Product Description</th><th>Cost per Item</th><th>Quantity</th>";
+              while ($row = mysqli_fetch_assoc($result)) {
+                  echo "<tr>";
+                  echo "<td>" . $row["productId"] . "</td><td>" . $row["productDescription"] . "</td><td>" . $row["costPerItem"] . "</td><td>" . $row["quantity"] . "</td>";
+                  echo "</tr>";
+              }
+              mysqli_free_result($result);
+              echo "</table>";
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <a class="waves-effect waves-light btn" href="index2.php">Go Back</a>
+  </body>
+</html>
