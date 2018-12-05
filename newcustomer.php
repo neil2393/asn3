6f50c133d70aa9eda5308ca7c2b192ebeeac26fe
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 19:39:55 -0500

    push scripts files

--- newcustomer.php
+++ newcustomer.php
@@ -47,7 +47,7 @@
 
               if ($customerId == 0 || $phoneNumber == 0 || $agentId == 0) {
                 echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
-                die("Error - Please make sure Customer ID, Phone Number, and Agent ID are integer values. Try again.");
+                die("Error - Please make sure you complete all values. Please make sure Customer ID, Phone Number, and Agent ID are integer values. Try again.");
               }
 
               $query = "SELECT * FROM customers";
@@ -68,49 +68,6 @@
                 die("Error - Please make sure your Agent ID exists. Try again.<br>" . mysqli_error($connection));
               }
               echo 'Your new customer was successfully added.';
-
-/*
-              $check = 0;
-              $query = 'SELECT * FROM purchases WHERE customerId = '. $customerId . ' AND productId = ' . $productId;
-              $result = mysqli_query($connection, $query);
-              if (!$result) {
-                  echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
-                  die("Error<br>" . mysqli_error($connection));
-              }
-              while ($row = mysqli_fetch_assoc($result)) {
-                  // If purchase exists, value of $purchase_exists will be > 0
-                  $check += $row['customerId'];
-              }
-              mysqli_free_result($result);
-
-              // If already purchased, update value
-              if($check > 0) {
-                  // Makes sure they are adding and not deducting
-                  if($quantity > 0){
-                      $query = 'UPDATE purchases SET quantity = quantity + ' . $quantity . ' WHERE customerId = '. $customerId . ' AND productId = ' . $productId;
-                      if (!mysqli_query($connection, $query)) {
-                          echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
-                          die("Error.<br>" . mysqli_error($connection));
-                      }
-                      echo "Your new purchase was successfuly updated.";
-                  }
-                  else {
-                    //  echo "Quantity should be positive!";
-                      echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
-                      die("Error - You can only enter positive quantities.");
-                  }
-              }
-              // Otherwise, insert values into purchases
-              else {
-                  $query = 'INSERT INTO purchases(customerId, productId, quantity) VALUES(' . $customerId . ',' . $productId . ',"' . $quantity . '")';
-                  if (!mysqli_query($connection, $query)) {
-                      echo "<a class='waves-effect waves-light btn' href='index2.php'>Go Back</a><br>";
-                      die("Error - Either the Customer ID or Product ID is incorrect.<br>" . mysqli_error($connection));
-                  }
-                  echo "Your new purchase was successfuly added.";
-              }
-
-  */
               mysqli_close($connection);
             ?>
           </div>
@@ -118,44 +75,20 @@
       </div>
     </div>
 
-
     <?php
-    /*
       echo "<blockquote><h5>Updated Customer Information:</h5></blockquote>"
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <?php
-              session_start();
-              include 'connectdb.php';
-
-              $customerId = (int)$_POST["customerId"];
-              $productId = (int)$_POST["productId"];
-              $quantity = (int)$_POST["quantity"];
-
-              $query = "SELECT * FROM products, purchases, customers WHERE products.productId = purchases.productId AND purchases.customerId = customers.customerId AND customers.customerId = " . $customerId;
-              //$query .= "customers.customerId = " . $customerId . ")";
-              $result = mysqli_query($connection,$query);
-              if (!$result) {
-                  die("databases query failed.");
-              }
-              echo "<table>";
-              echo "<tr><th>Product ID</th><th>Product Description</th><th>Cost per Item</th><th>Quantity</th>";
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo "<tr>";
-                  echo "<td>" . $row["productId"] . "</td><td>" . $row["productDescription"] . "</td><td>" . $row["costPerItem"] . "</td><td>" . $row["quantity"] . "</td>";
-                  echo "</tr>";
-              }
-              mysqli_free_result($result);
-              echo "</table>";
-            ?>
+    ?>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <?php
+              include 'getcustomerdata.php';
+              ?>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    */
-    ?>
 
     <a class="waves-effect waves-light btn" href="index2.php">Go Back</a>
   </body>
