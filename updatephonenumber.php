0e94d9251bbcd935342610d40d362c8f2e45e80a
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-05 00:58:38 -0500

    push scripts files

--- updatephonenumber.php
+++ updatephonenumber.php
@@ -42,7 +42,7 @@
       echo "<blockquote><h5>Customer " . $customerId . " Phone Number Information:</h5></blockquote>"
     ?>
     <div class="row">
-      <div class="col s12 m6">
+      <div class="col s12 m4">
         <div class="card blue-grey darken-1">
           <div class="card-content white-text">
             <?php
