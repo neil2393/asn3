6c8a20106233c52f065dac100875bc1fb72b48bd
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-05 00:56:36 -0500

    push scripts files

--- updatephonenumber.php
+++ updatephonenumber.php
@@ -53,10 +53,10 @@
                   die("databases query failed.");
               }
               echo "<table>";
-              echo "<tr><th>Customer ID</th><th>First Name</th><th>Last Name</th><th>City</th><th>Phone Number</th><th>Agent ID</th></tr>";
+              echo "<tr><th>Customer ID</th><th>Phone Number</th></tr>";
               while ($row = mysqli_fetch_assoc($result)) {
                   echo "<tr>";
-                  echo "<td>" . $row["customerId"] . "</td><td>" . $row["firstName"] . "</td><td>" . $row["lastName"] . "</td><td>" . $row["city"] . "</td><td>" . $row["phoneNumber"] . "</td><td>" . $row["agentId"] . "</td>";
+                  echo "<td>" . $row["customerId"] . "</td><td>" . $row["phoneNumber"] . "</td>";
                   echo "</tr>";
               }
               mysqli_free_result($result);
