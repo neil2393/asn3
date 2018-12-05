9c9ebbea7e85e49a5fbe0e90a26f9c530c511398
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-04 19:05:38 -0500

    push scripts files

--- newcustomer.php
+++ newcustomer.php
@@ -114,8 +114,8 @@
 
 
     <?php
+    /*
       echo "<blockquote><h5>Updated Customer Information:</h5></blockquote>"
-    ?>
     <div class="row">
       <div class="col s12 m6">
         <div class="card blue-grey darken-1">
@@ -148,6 +148,8 @@
         </div>
       </div>
     </div>
+    */
+    ?>
 
     <a class="waves-effect waves-light btn" href="index2.php">Go Back</a>
   </body>
