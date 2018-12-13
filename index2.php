127eae86c7492ed72533ba7fd954b2dd52bf2c8a
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-13 16:34:32 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -37,7 +37,7 @@
       <br>
       <blockquote><h5>Customer Information:</h5></blockquote>
       <div class="row">
-        <div class="col s12 m6">
+        <div class="col s12 m4">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
               <?php
