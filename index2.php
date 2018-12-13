13ab2ebb5420b0c8e6b10dfd6111380d9446b119
Neil Patel <neilpatel@Kyles-MacBook-Pro.local>
2018-12-13 16:39:07 -0500

    push scripts files

--- index2.php
+++ index2.php
@@ -50,7 +50,7 @@
 
       <blockquote><h5>Products that a Customer has Purchased:</h5></blockquote>
       <div class="row">
-        <div class="col s12 m6">
+        <div class="col s12 m8">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
               <span class="card-title">Customer Purchase Information</span>
